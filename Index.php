7996e2491839d9e87a7c74d8cc6fe30853965cf8
Atokareva12 <at120802@mail.ru>
2022-01-27 21:42:34 +0300

    Добавлен  привет

---
+++ Index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <dif class="Zadanie">
+          <h2>Задание 1</h2>
+          Создайте переменные $a=17 и $b=10. Отнимите от $a переменную $b и
+результат присвойте переменной $c. Затем создайте переменную $d,
+присвойте ей значение 7. Сложите переменные $c и $d, а результат запишите
+в переменную $result. Выведите на экран значение переменной $result. 
+     </dif>
+    <dif class="Otvet">
+
+    <?php
+         $a=17;
+         $b=10;
+         $c=$a-$b;
+         $d=7;
+         $result=$c+$d;
+    ?>    
+         Ответ: <?=$result ?>   
+    </dif>
+
+
+
+
+
+
+
+</body>
+</html>
