246c9002850133b185115aab5edfc420041acd0c
Atokareva12 <at120802@mail.ru>
2022-01-31 17:35:35 +0300

    строка 112 изменения

--- index.php
+++ index.php
@@ -109,20 +109,18 @@
 </div>
     <div class="Otvet"> 
     <?php
-    let min​=+​prompt​(​"Введите число от 0 до 59");
-    if (min >= 0 && min <= 14) {
-      console.log ("Первая");
-   }
-    if (min >= 15 && min <= 30) {
-      console.log ("Вторая");
-   }
-   if (min >= 31 && min <= 45) {
-      console.log ("Третья");
-   }
-   if (min >= 46 && min <= 59) {
-      console.log ("Четвертая");
-   }
-      </script>
+    
+    g=int(input())
+
+    if g>=45: print("Число входит в 4 четверть часа")
+    
+    elif g>=30: print("Число входит в 3 четверть часа")
+    
+    elif g>=15: print("Число входит в 2 четверть часа")
+    
+    else: print("Число входит в 1 четверть часа")
+    
+    
   ?>
         Ответ:<?=$result ?>  
   </div>
