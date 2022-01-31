d03902a591cbf502991e2fe3cda09d19732fbd6e
Atokareva12 <at120802@mail.ru>
2022-01-31 16:32:33 +0300

    Сделала задание 8,начала 9

--- index.php
+++ index.php
@@ -128,4 +128,40 @@
   </div>
 
 
+    <dif class="Zadanie">
+          <h2>Задание 8</h2>
+Переменная $num может принимать одно из значений: 1, 2, 3 или 4.
+Если она имеет значение '1', то в переменную $result запишем 'зима', если
+имеет значение '2' – 'лето' и так далее. Решите задачу через switch-case.
+</div>
+     <div class="Otvet"> 
+     <?php
+ $num = 2;
+ switch ($num) {
+      case 1:
+           $result = 'зима';
+           echo $result;
+           break;
+      case 2:
+           $result = 'лето';
+           echo $result;
+           break;
+      case 3:
+           $result = 'осень';
+           echo $result;
+           break;
+      case 4:
+           $result = 'весна';
+           echo $result;
+           break;		
+      default:
+           echo "Введите число от 1 до 4 ";
+           break;
+      }
+ 
+ ?>
+      Ответ:<?=$result ?>  
+  </div>
+
+
   
