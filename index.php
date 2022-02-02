952934b7d9c691cab3bdc955f17fbe34b19829c6
Atokareva12 <at120802@mail.ru>
2022-02-02 16:00:30 +0300

    изменения для себя

--- index.php
+++ index.php
@@ -365,4 +365,9 @@ i = i + 1; /* Прибавляем к значению счётчика един
      
 
 
-
+     <!-- As a link -->
+     <nav class="navbar navbar-light bg-light">
+       <div class="container-fluid">
+         <a class="navbar-brand" href="#">Navbar</a>
+       </div>
+     </nav>
