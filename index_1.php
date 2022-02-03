47adaef317b33b4409124f3c7f740a79fee7370a
Atokareva12 <at120802@mail.ru>
2022-02-03 21:11:38 +0300

    из

--- index_1.php
+++ index_1.php
@@ -40,11 +40,6 @@ col-xs-12
 <div class="row">
 <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">Левое меню</div>
 <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">Контент</div>
-
-<?
-echo "<div class='priv'>туту</div>";
-?>
-
 </div>
 <!-- 3 строка Подвал сайта - на всех устройствах поделен на две части по
 шесть колонок--> 
