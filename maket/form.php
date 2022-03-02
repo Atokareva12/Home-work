9ac3974f35d418b251e9f93fa11f46f3e908e04d
Atokareva12 <At120802@mail.ru>
2022-03-02 14:06:48 +0700

    форма

---
+++ maket/form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylecheet' href="css/bootstrap.css">
+    <link rel='stylecheet' href="css/style.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <p> Введите имя </p>
+    <form action="form.php" 
+
+
+
+
+</body>
+</html>
