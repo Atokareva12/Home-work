cca833e33fb3fc05dc946b035b5c5350f52986f1
Atokareva12 <at120802@mail.ru>
2022-03-08 12:19:39 +0300

    коммит

---
+++ maket/index.php
@@ -0,0 +1,49 @@
+<?php
+        require "head.html";
+    ?>
+    
+    <div class="container-fluid">
+       
+            <!-- Фото сайта -->
+        <section class="row">
+            <div class="col-md-12 photo block">Фото</div>
+        </section>
+            <!-- Товары -->
+    
+        <section class="product row">
+            <div class="col-md-12 block">Товары</div>
+        </section>    
+        
+            <!-- Первая строка с товарами -->
+        <section class="row">
+            <div class="col-md-2 col-md-offset-3 block products">Продукт</div>
+            <div class="col-md-2 col-md-offset-2 block products">Продукт</div>
+        </section>
+        <div class="row">
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+            <div class="col-md-1 block">fsdfs</div>
+        </div>
+            <!-- Вторая строка с товарами -->
+        <section class="row">
+            <div class="col-md-2 col-md-offset-1 block products">Продукт</div>
+            <div class="col-md-2 col-md-offset-1 block products">Продукт</div>
+            <div class="col-md-2 col-md-offset-1 block products">Продукт</div>
+            <div class="col-md-2 col-md-offset-1 block products">Продукт</div>
+        </section>
+        <?php
+            require "footear.html";
+        ?>
+       
+    </div>
+</body>
+</html>
