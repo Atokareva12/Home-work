e3deadb86b147f59b4ee2910ffa42af2067fb68e
Atokareva12 <at120802@mail.ru>
2022-03-08 15:24:01 +0300

    исправление

--- Bootstrap/independentBootstrap2_1.php
+++ Bootstrap/independentBootstrap2_1.php
@@ -1,12 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+    require "inc/header.php";
+?>
+<div class="row">
+    <div class="col-md-2 coll">1</div>
+    <div class="col-md-1 coll">2</div>
+    <div class="col-md-2 coll">3</div>
+    <div class="col-md-1 coll">4</div>
+    <div class="col-md-2 coll">5</div>
+    <div class="col-md-1 coll">6</div>
+    <div class="col-md-2 coll">7</div>
+    <div class="col-md-1 coll">8</div>
+</div>
+
+<div class="row">
+    <div class="col-md-12 coll1">9</div>
+</div>
+
+<div class="row">
+    <div class="col-md-4 coll2">10</div>
+    <div class="col-md-4 coll2">11</div>
+    <div class="col-md-4 coll2">12</div>
+</div>
+
+<div class="row">
+    <div class="col-md-11 coll3">13</div>
+    <div class="col-md-1 coll3">14</div>
+</div>
+<?php
+    require "inc/footer.php";
+?>
---
+++ Bootstrap/independentBootstrap2_2.php
@@ -0,0 +1,22 @@
+<?php
+    require 'inc/header.php';
+?>
+ <div class="row row1">Основной</div>
+        <div class="row row1"> 
+            <div class="col-md-2">col-md-2</div>
+            <div class="col-md-2 col-md-offset-3">col-md-2</div>
+            <div class="col-md-2 col-md-offset-3">col-md-2</div>
+        </div>
+    
+        <div class="row row2">
+            Основной 2
+            <div class="col-md-2 col-md-offset-10">col-md-2</div>
+        </div>
+        <div class="row row3">
+            <div class="col-md-2 ">col-md-2</div>
+            <div class="col-md-2 ">col-md-2</div>
+        </div>
+        
+<?php 
+    require 'inc/footer.php';
+ ?>
